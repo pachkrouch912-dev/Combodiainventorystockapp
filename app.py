bf32743069389e59c75181634216dd1109373720
pachkrouch912-dev <pachkrouch912@gmail.com>
2026-08-01 21:19:00 +0700

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 app.secret_key = "cambodia-inventory-secure-secret-key"
 
 SUPABASE_URL = "https://dwqyrlrylworstasglsi.supabase.co"
-SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR3cXlybHJ5bHdvcnN0YXNnbHNpIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc4FSZDNDU3NzAxLCJleHAiOjIxMDEwODE3NzB9.gR5rqaHs44_4pH-ufkdRRhsx1rt2jEAnP1d905Go5Rc" # (ប្រើ key ដើមរបស់បង)
+SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR3cXlybHJ5bHdvcnN0YXNnbHNpIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc4NTUwNTc3MCwiZXhwIjoyMTAxMDgxNzcwfQ.gR5rqaHs44_4pH-ufkdRRhsx1rt2jEAnP1d905Go5Rc" # (ប្រើ key ដើមរបស់បង)
 
 def supabase_request(endpoint, method="GET", data=None):
     url = f"{SUPABASE_URL}/rest/v1/{endpoint}"
